9f3192026d8c5c33bf60d295dcaf125a4173159b
wiandamumtaz <wianda_mumtaz_29rpl@student.smktelkom-mlg.sch.id>
2022-01-19 07:21:22 +0700

    latsoljava

---
+++ 1.js
@@ -0,0 +1,14 @@
+let lebar = 30;
+let panjang = 20.5;
+let harga = 1500000;
+let sementara =0
+let total =0
+
+luas = lebar*panjang
+sementara = luas*harga
+
+let ppn = 0
+ppn = sementara * 15/100
+total =+ sementara + ppn
+
+console.log("total ="+total);
